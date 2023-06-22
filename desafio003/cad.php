d830b7d3f59aeb676bac8481f2477560dd92db47
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2023-06-22 15:56:32 -0300

    conserto

--- desafio003/cad.php
+++ desafio003/cad.php
@@ -10,13 +10,19 @@
     <main>
         <h1>Conversor de Moedas v1.0</h1>
         <?php 
-           (float)$num = $_GET["num"] ?? "error"; 
+           (float)$num = $_GET["num"] ?? 0; 
            $dolar = $num / 4.77;
            
-           echo "<p>Seus R$$num equivalem a US$ ".round($dolar, 2)."</p>";
-           echo "*<strong>Cotação fixa de R$5,22</strong> informada diretamente no código."
+           //echo "<p>Seus R$". number_format($num, 2, ",", ".") ." equivalem a US$ ".number_format($dolar, 2, ",", ".")."</p>";
+           // formatação com internacionalização
+           
+           $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+           echo "<p>Seus ". numfmt_format_currency($padrao, $num, "BRL") ." equivalem a ". numfmt_format_currency($padrao, $dolar, "USD") ."</p>";
+
+
+           echo "*<strong>Cotação fixa de R$5,22</strong> informada diretamente no código.";
         ?>
-        <p><a href="javascript:history.go(-1)"> <-- Voltar</a></p>
+         <button onclick="javascript:history.go(-1)">&#129044; Voltar</button>
     </main>
 </body>
 </html>
